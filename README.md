7784582d178b0ff8dff354be54d34c112d8883c5
Archer Stéphane <2981437+stephane-archer@users.noreply.github.com>
2023-01-16 13:19:04 -0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# diffdate
+
+get the difference in days between two dates
+
+usage is:
+diffdate 12-01-2023 22-12-2022
---
+++ src/diffdate/diffdate.py
@@ -0,0 +1,23 @@
+import sys
+import datetime
+
+def printusage():
+    print("usage is: diffdate 12-01-2023 22-12-2022")
+
+if len(sys.argv) != 3:
+    printusage()
+    exit()
+
+dates = []
+for date in sys.argv[1:]:
+    s = date.split('-')
+    if len(s) != 3:
+        printusage()
+        exit()
+    dates.append(datetime.date(int(s[2]),int(s[1]),int(s[0])))
+
+if len(dates) != 2:
+    printusage()
+    exit()
+
+print((dates[0] - dates[1]).days)
