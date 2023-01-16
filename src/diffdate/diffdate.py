9836fd2cb73d7ea1202457c037332932d5ce3930
Archer Stéphane <2981437+stephane-archer@users.noreply.github.com>
2023-01-16 15:54:34 -0300

    make some change to make it a cli tool

--- src/diffdate/diffdate.py
+++ src/diffdate/diffdate.py
@@ -4,20 +4,24 @@ import datetime
 def printusage():
     print("usage is: diffdate 12-01-2023 22-12-2022")
 
-if len(sys.argv) != 3:
-    printusage()
-    exit()
-
-dates = []
-for date in sys.argv[1:]:
-    s = date.split('-')
-    if len(s) != 3:
+def main():
+    if len(sys.argv) != 3:
         printusage()
         exit()
-    dates.append(datetime.date(int(s[2]),int(s[1]),int(s[0])))
 
-if len(dates) != 2:
-    printusage()
-    exit()
+    dates = []
+    for date in sys.argv[1:]:
+        s = date.split('-')
+        if len(s) != 3:
+            printusage()
+            exit()
+        dates.append(datetime.date(int(s[2]),int(s[1]),int(s[0])))
 
-print((dates[0] - dates[1]).days)
+    if len(dates) != 2:
+        printusage()
+        exit()
+
+    print((dates[0] - dates[1]).days)
+
+if __name__ == "__main__":
+    main()
